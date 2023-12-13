e7729b98c02140ce8413256d385554e60176b734
gruud <6158418+gruud@users.noreply.github.com>
2023-12-13 20:54:11 +0100

    Bill splitter Step 2

--- bill_splitter.py
+++ bill_splitter.py
@@ -1,16 +1,36 @@
 """Hyperskill Bill-Splitter project implementation"""
 
 
-def main():
-    """Main program."""
+def get_friends_list():
+    """Constructs a dictionaries with friends present at the party.
+
+    If the number of friends is lower or equal to zero, returns
+    an empty list
+    """
+    friends = {}
     friends_count = int(input("Enter the number of friends joining (including you):"))
-    if friends_count <= 0:
-        print("No one is joining for the party")
-    else:
-        friends = {}
+    if friends_count > 0:
         print("Enter the name of every friend (including you), each on a new line:")
         for i in range(0, friends_count):
             friends[input()] = 0
+    return friends
+
+
+def split_bill(friends):
+    """Compute de bill's fraction for each friend and update the dictionary."""
+    bill = float(input("Enter the total bill value:"))
+    split_amount = round(bill / len(friends), 2)
+    for name, amount in friends.items():
+        friends[name] = split_amount
+
+
+def main():
+    """Main program."""
+    friends = get_friends_list()
+    if len(friends) <= 0:
+        print("No one is joining for the party")
+    else:
+        split_bill(friends)
         print(friends)
 
 
